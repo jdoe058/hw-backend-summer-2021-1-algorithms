ca2bcae9fac3dcd1e105bd88dce7699be581d110
Zheka Doe <jdoe058@gmail.com>
2021-08-17 22:05:23 +0300

    solve lesson 5

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,4 +7,11 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if (number == 0) or (number == 1):  # частный случай
+        return False
+
+    k = 0
+    for i in range(2, number // 2 + 1):
+        if not (number % i):
+            k += 1
+    return not k
--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,8 +12,30 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    seconds_in_minute = 60
+    seconds_in_hour = 60 * seconds_in_minute
+    seconds_in_day = 24 * seconds_in_hour
 
+    tmp_str = ''
+    tmp = seconds // seconds_in_day
+    if tmp:
+        tmp_str += f'{tmp:02}d'
+        seconds %= seconds_in_day
 
+    tmp = seconds // seconds_in_hour
+    if tmp:
+        tmp_str += f'{tmp:02}h'
+        seconds %= seconds_in_hour
+    elif tmp_str:
+        tmp_str += '00h'
 
+    tmp = seconds // seconds_in_minute
+    if tmp:
+        tmp_str += f'{tmp:02}m'
+        seconds %= seconds_in_minute
+    elif tmp_str:
+        tmp_str += '00m'
 
+    tmp_str += f'{seconds:02}s'
+
+    return tmp_str
