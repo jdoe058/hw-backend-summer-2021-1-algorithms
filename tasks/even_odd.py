319a57556a678ba73e190f3b43ff12a2520a6ba4
Zheka Doe <jdoe058@gmail.com>
2021-08-17 23:07:20 +0300

    solve lesson 6.1 even_odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,4 +9,15 @@ def even_odd(arr: list[int]) -> float:
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    raise NotImplementedError
+    odd = 0
+    even = 0
+    for i in arr:
+        if i & 1:
+            odd += i
+        else:
+            even += i
+
+    if odd:
+        return even / odd
+    else:
+        return 0
